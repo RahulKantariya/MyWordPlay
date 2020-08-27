f8cc9086a5c009739a2f58dc99f36913cf3b4c71
Rahul <69952584+RahulKantariya@users.noreply.github.com>
2020-08-27 12:50:28 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def gettext():
-    return render_template('./templates/index.html')
+    return render_template('index.html')
 
 @app.route('/translator',methods=['POST','GET'])
 def translator():
